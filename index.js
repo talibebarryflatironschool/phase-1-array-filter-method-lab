857e361c1e9bd41229aa8d0b9ded933e0d5af200
Mamoudou Barry <barrymamoudou982@gmail.com>
2025-01-08 15:38:33 -0500

    filter lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driverNames,name ){
+    return driverNames.filter(( driver )=>{
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+function fuzzyMatch(driverNames,letters){
+    return driverNames.filter((driver)=>{
+        return driver.slice(0, letters.length) === letters
+     })
+}
+function matchName(driverObjects, name){
+    return driverObjects.filter(( driver )=>{
+        return driver.name === name
+    })
+}
